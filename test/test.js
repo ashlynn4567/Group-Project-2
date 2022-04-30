6689265f011ed49cc36be7c8771b2da4226677a4
Ashley's PC <ashlynn4567@gmail.com>
2022-04-29 22:56:25 -0500

    initial file setup according to MVC

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// test to ensure mocha is installed and running properly
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
