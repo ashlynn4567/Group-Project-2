25d9920455c3fabd1f07783ed667958e71863080
Kelsey Alderman <kelseyalderman@gmail.com>
2022-05-02 18:38:24 -0700

    Create Notebook API Routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,4 +7,9 @@ const userRoutes = require("./user-routes");
 
 router.use("/users", userRoutes);
 
+// prefixes routes in notebook-routes.js with the path /notebooks
+const notebookRoutes = require("./notebook-routes");
+
+router.use("/notebooks", notebookRoutes);
+
 module.exports = router;
