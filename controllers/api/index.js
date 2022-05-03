ef52e1737f963e4e46d415f72d7103e9988f53c8
Kelsey Alderman <kelseyalderman@gmail.com>
2022-05-02 19:12:39 -0700

    Create Note API Routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,12 +4,14 @@ const router = require("express").Router();
 
 // prefixes routes in user-routes.js with the path /users
 const userRoutes = require("./user-routes");
-
 router.use("/users", userRoutes);
 
 // prefixes routes in notebook-routes.js with the path /notebooks
 const notebookRoutes = require("./notebook-routes");
-
 router.use("/notebooks", notebookRoutes);
+
+// prefixes routes in note-routes.js with the path /notes
+const noteRoutes = require("./note-routes");
+router.use("/notes", noteRoutes);
 
 module.exports = router;
