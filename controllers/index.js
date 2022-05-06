69a5de8ba9ea04bdfe586c09906c99e400eccb24
Kelsey Alderman <kelseyalderman@gmail.com>
2022-05-05 18:46:16 -0700

    create homepage and mynotebooks routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,12 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
+const mynotebooksRoutes = require("./mynotebooks-routes.js");
 
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/mynotebooks", mynotebooksRoutes);
 
 // If we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource
 router.use((req, res) => {
---
+++ controllers/mynotebooks-routes.js
@@ -0,0 +1,102 @@
+const router = require("express").Router();
+const { User, Notebook, Note } = require("../models");
+
+// get all notebooks for My Notebooks
+router.get("/", (req, res) => {
+  Notebook.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "notebook_name", "created_at"],
+    include: [
+      {
+        model: Note,
+        attributes: ["id", "question", "answer", "notebook_id"],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((notebookData) => {
+      const notebooks = notebookData.map((notebook) =>
+        notebook.get({ plain: true })
+      );
+      res.render("my-notebooks", { notebooks, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get a single notebook (view notebook)
+router.get("notebooks/:id", (req, res) => {
+  Notebook.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "notebook_name", "created_at"],
+    include: [
+      {
+        model: Note,
+        attributes: ["id", "question", "answer", "notebook_id"],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((notebookData) => {
+      if (!notebookData) {
+        res.status(404).json({ message: "No notebook found with this id" });
+        return;
+      }
+      const notebook = notebookData.get({ plain: true });
+
+      res.render("single-notebook", {
+        notebook,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// edit notebook
+router.get("/edit/:id", (req, res) => {
+  Notebook.findByPk(req.params.id, {
+    attributes: ["id", "notebook_name", "created_at"],
+    include: [
+      {
+        model: Note,
+        attributes: ["id", "question", "answer", "notebook_id"],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((notebookData) => {
+      if (notebookData) {
+        const notebook = notebookData.get({ plain: true });
+
+        res.render("edit-notebook", {
+          notebook,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
