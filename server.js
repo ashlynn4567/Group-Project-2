a24a0d422ee7e84f91141a1b2ac84dd95812f3a3
Ashley's PC <ashlynn4567@gmail.com>
2022-05-08 14:40:18 -0500

    added login and logout js, also connected public folder to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,6 +22,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
